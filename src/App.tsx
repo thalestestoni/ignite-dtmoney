16a1c0bf61630c4b5c45a32686d9ad3161b5ea6d
Thales Testoni <thalestestoni@gmail.com>
2021-04-06 20:27:07 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-export function App() {
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
