088f5695865a7265fac526453be282090489e873
Thales Testoni <thalestestoni@gmail.com>
2021-04-10 13:47:54 -0300

    Componente summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
